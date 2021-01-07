800b22b8d96084557869a41a832fea96e64a6b33
Jim Carroll <jim@pulamusic.com>
2021-01-07 17:59:53 -0500

    added a postgresql db

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express'),
   path = require('path'),
   cool = require('cool-ascii-faces'),
+  { Pool } = require('pg'),
   port = process.env.PORT || 5000
+
+const pool =  new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,4 +17,18 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(port, () => console.log(`Listening on ${port}`))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect(),
+        result = await client.query('SELECT * FROM test_table'),
+        results = {'results': (result) ? result.rows : null}
+      res.render('pages/db', results)
+      client.release()
+    } catch (err) {
+      console.error(err)
+      res.send('Error: ' + err)
+    }
+  })
+  .listen(
+    port, () => console.log(`Listening on ${port}`)
+  )
