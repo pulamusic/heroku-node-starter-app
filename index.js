216c16f73a43d40b4b24c16bbb9016825926f449
Jim Carroll <jim@pulamusic.com>
2021-01-07 16:20:49 -0500

    fixed some glitches

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
 const express = require('express'),
   path = require('path'),
-  cool = require('cool-ascii-faces')
-  port = process.env.PORT || 5000
+  cool = require('cool-ascii-faces'),
+  dotenv = require('dotenv')
 
-  /*
-  dotenv = require('dotenv'),
-  result = dotenv.config(),
+const result = dotenv.config(),
+  port = process.env.PORT
+
 if (result.error) {
   throw result.error
 }
-*/
+
 express()
-  .use(
-    express.static(
-      path.join(__dirname, 'public')
-    )
-  )
-  .set(
-    'views',
-    path.join(__dirname, 'views')
-  )
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get(
-    '/',
-    (req, res) => res.render('pages/index')
-  )
-  .get(
-    '/cool',
-    (req, res) => res.send(
-      cool()
-    )
-  )
-  .listen(
-    port,
-    () => console.log(`Listening on ${port}`)
-  )
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(port, () => console.log(`Listening on ${port}`))
+
